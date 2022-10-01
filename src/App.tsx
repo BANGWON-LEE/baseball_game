548655641bd78221799ec4258aff62b77eeb771d
BANGWON-LEE <qkddnjs31@gmail.com>
2022-10-01 17:59:33 +0900

    20221001 그라운드 화면 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from './Components/Main';
+import Main from './components/Main';
+import Ground from './components/Ground';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <Main />
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route  element={<Main />} path="/" />
+          <Route  element={<Ground />} path="/game" />
+        </Routes>
+      </BrowserRouter> 
   );
 }
 
