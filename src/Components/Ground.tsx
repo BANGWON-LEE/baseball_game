b5e1a25eb92af28e86fb20a4c43db01cff00e4d4
BANGWON-LEE <qkddnjs31@gmail.com>
2022-10-02 19:09:57 +0900

    20221002 ground 컴포넌트 ui 추가 및 main 컴포넌트 코드 추가

--- src/Components/Ground.tsx
+++ src/Components/Ground.tsx
@@ -1,47 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import '../styles/component.scss'
 
 const Ground: React.FC = () => {
 
-  return(
-    <div className="total_back">
-        <div className="game_board">
-            <div className="game_board_detail">
-                <div className="game_board_detail_home">
-                    <span className="game_board_detail_team home-team">우리 야구단</span>
-                    <span className="game_board_detail_score home-team">1</span>
-                </div>
-                <span className="game_board_detail_vs">VS</span>
-                <div className="game_board_detail_away">
-                    <span className="game_board_detail_score away-team">1</span>
-                    <span className="game_board_detail_team away-team">너네 야구단</span>
-                </div>
-            </div>
-        </div>
-        <div className="ground_back">
-            <div className="ground_back_position">
-                <div className="ground_back_position_location">
-                    <div className="ground_back_position_location_first-base">
+    const [searchParams] = useSearchParams();
+    const teamName = searchParams.get('team');
+    const [gameStatus, setGameStatus] = useState<Number>(0)
+    const gameStart = () :void => {
+        setGameStatus(+1)
+    }
 
+
+
+    return(
+        <div className="total_back">
+            <div className="game_board">
+                <div className="game_board_detail">
+                    <div className="game_board_detail_home">
+                        <span className="game_board_detail_team home-team">{teamName}</span>
+                        <span className="game_board_detail_score home-team">1</span>
                     </div>
-                    <div className="ground_back_position_location_second-base">
-                        
-                    </div>
-                    <div className="ground_back_position_location_third-base">
-                        
-                    </div>
-                    <div className="ground_back_position_location_last-base">
-                        
-                    </div>
-                    <div className="ground_back_position_location_pitcher-base">
-                        
+                    <span className="game_board_detail_vs">VS</span>
+                    <div className="game_board_detail_away">
+                        <span className="game_board_detail_score away-team">1</span>
+                        <span className="game_board_detail_team away-team">너네 야구단</span>
                     </div>
                 </div>
             </div>
+            <div className="ground_back">
+                <div className="ground_back_position">
+                    <div className="ground_back_position_top-base"/>
+                    <div className="player_located_first_base"/>
+                    <div className="ground_back_position_right-base"/>
+                    <div className="player_located_right_base"/>
+                    <div className="ground_back_position_left-base"/>
+                    <div className="player_located_left_base"/>
+                    <div className="ground_back_position_home-base"/>
+                    <div className="player_located_last_base home_color"/>
+                    <div className="ground_back_position_pitcher-base"/>
+                    {gameStatus === 1 &&
+                    <button className="game_start" onClick={gameStart}>게임시작</button>
+                    }
+                </div>
+            </div>
         </div>
-    </div>
-  )
-  
+    )
 }
 
 export default Ground;
--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,26 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useRoutes } from "react-router-dom";
 import '../styles/component.scss'
 
 const Main: React.FC = () => {
+
+
+  const [teamName, setTeamName] = useState<String>('');
+  const [resultName, setResultName] = useState<Boolean>(false);
+  
+  const registerTeam = () : void => {
+    setResultName(true);
+  }
+  const changeTeamName = () : void => {
+    setResultName(false);
+    setTeamName('')
+  }
 
   return(
     <div className="main_back">
       <div className="main_block">
         <div className="project_title">
           <h1>Baseball Game</h1>
+          {resultName === true &&
+          <>
+            <h5>{teamName}</h5>
+            <div className="project_title_change">
+              <button onClick={changeTeamName} className="project_title_change_team_name">
+                팀명 변경하기
+              </button>
+            </div>
+          </>
+          }
         </div>
         <div className="project_content">
           <div>
-            <input type="text" placeholder="팀명을 입력하세요." className="project_content_title" />
-            <button className="project_content_btn">
-              등록
-            </button>
+            {resultName === false &&
+              <>
+                <input 
+                  type="text" 
+                  placeholder="팀명을 입력하세요." 
+                  className="project_content_title"   
+                  onChange={(e) => setTeamName(e.target.value)} 
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      registerTeam();
+                    }
+                  }}
+                />
+                <button onClick={registerTeam} className="project_content_btn">
+                  등록
+                </button>
+              </> 
+            }
           </div>
         </div>
         <div className="project_bottom">
-          <button className="btn_game_start">
-            게임시작
-          </button>
+        {resultName === true &&
+          <Link to={`/game?team=${teamName}`}>
+            <button className="btn_game_start">
+              게임시작
+            </button>
+          </Link>
+        }
         </div>
       </div>
     </div>
