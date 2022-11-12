fc29ef264c3fbd301ab6e707ddaa141245983d8c
BANGWON-LEE <qkddnjs31@gmail.com>
2022-11-12 17:27:40 +0900

    20221112 번호 입력 계산기 ui 수정 및 코드 수정

--- src/Components/Ground.tsx
+++ src/Components/Ground.tsx
@@ -11,65 +11,152 @@ const Ground: React.FC = () => {
     const [cntSecond, setCntSecond] = useState<string>('')
     const [cntThird, setCntThird] = useState<string>('')
 
-    // useEffect(() => {
-    //     if(cntFirst[0] ||cntSecond[0] || cntThird[0]  === '0'){
-    //         cntFirst.substring(1, 1)
-            
-    //     }  
+    interface BtnType
+        {
+            num : Line[];
+            // middle : LineMiddle[];
+            // bottom : LineBottom[];
+            // zero : LineFinish[];
+        }
 
-    //     console.log('repl', cntFirst)
-    // },[cntFirst, cntSecond, cntThird])
+    interface Line
+        {
+            no1: string;
+            no2: string;
+            no3: string;
+        };
 
+
+    const numObject: BtnType = {
+        num:[
+            {
+                no1:'1',
+                no2:'2',
+                no3:'3'
+            },
+            {
+                no1:'4',
+                no2:'5',
+                no3:'6'
+            },
+            {
+                no1:'7',
+                no2:'8',
+                no3:'9'
+            },
+            {
+                no1:'',
+                no2:'0',
+                no3:''
+            },
+        ],
+        // middle:[
+        //     {
+        //         no1:'4',
+        //         no2:'5',
+        //         no3:'6'
+        //     },
+        // ],
+        // bottom:[
+        //     {
+        //         no1:'7',
+        //         no2:'8',
+        //         no3:'9'
+        //     },
+        // ],
+        // zero:[
+        //     {no1:'0'}
+        // ]
+    };
+
+    // const [myNumArray, setMyNumArray] = useState
+    const  [myNumArray, setMyNumArray] =useState<string[]>([]) // 숫자 배열
+
+    const gameStart = () : void => {
+        
+        if(gameStatus === 2){
+            if((myNumArray[0] === '0' || myNumArray[1] === '0' || myNumArray[2] === '0') ||   (cntFirst <= '0'  &&  cntFirst > '15') ){
+                alert('알맞은 숫자를 입력해주세요');
+                return
+            }
+            // outCntArray.push(ZeroRemove(cntFirst))
+            // outCntArray.push(ZeroRemove(cntSecond))
+            // outCntArray.push(ZeroRemove(cntThird))
+        }
+        setGameStatus(gameStatus + 1)
+    }
+
+        // 새로고침 시에 등장할 경고문구
     const alertUser = (event: any) => {
         event.preventDefault();
         event.returnValue = "";
     }
 
+    // 새로고침 시에 등장할 경고문구
     useEffect(() => {
         window.addEventListener("beforeunload", alertUser);
         return () => {
             window.removeEventListener("beforeunload", alertUser);
         };
+        
     }, []);
 
-    // const [outCntArray, setOutCntArray] = useState<String[]>()
-    const outCntArray :any = []
 
-    const ZeroRemove = (no: any):void =>{
-        return no.replace('0', '')
-    }
-    
-    const gameStart = () :void => {
-        
-        if(gameStatus === 2){
-            if((cntFirst || cntSecond || cntThird) === '' || (cntFirst || cntSecond || cntThird) > '15' ){
-                alert('알맞은 숫자를 입력해주세요');
-                return
-            }
-            outCntArray.push(ZeroRemove(cntFirst))
-            outCntArray.push(ZeroRemove(cntSecond))
-            outCntArray.push(ZeroRemove(cntThird))
-        }
+    // 계산기 버튼 클릭시 보여지는 부분 정리
+    // const [selectedNum, setSelectedNum] = useState<string>('')
 
-        setGameStatus(gameStatus + 1)
-    }
-    
-    const [choiceNum, setChoiceNum] = useState<string>('')
     const selectNum = (event:any) :void => {
-        if(document.getElementById('input_first')?.focus){
-        setCntFirst(cntFirst.concat(event.currentTarget.value))
-        }
+        const numberChoice = event.currentTarget.value
+
+            setCntFirst(cntFirst.concat(numberChoice))
+       
+    }
+    
+    const toNextNum = () : void => {
+        setMyNumArray(myNumArray.concat(cntFirst))
+        setCntFirst('')
     }
 
-    useEffect(() => {
+    const toPrevNum = () : void => {
+        // myNumArray.pop()
+        if(myNumArray.length === 2){
+            setCntFirst(myNumArray[1])
+        } else if( myNumArray.length === 1 ){
+            setCntFirst(myNumArray[0]) 
+        }
+      
+        // setCntFirst('')
+    }
+    
+    console.log('myNumArray', myNumArray)
 
-    },[])
 
+    
+    // useEffect(() => {
+    //     console.log('selectedNum', selectedNum)
+    //     if(selectedNum){
 
+    //         if(cntFirst){
+    //             setCntFirst(cntFirst.concat(selectedNum))
+   
+    //             if(cntFirst.length <= 2){
+    //                 setCntFirst(cntFirst.substring(1))
+    //                 // document.getElementById('insert_first')?.blur()
+    //                 document.getElementById('insert_second')?.focus()
+    //             } 
+    //         } else if(cntSecond){
+    //             setCntSecond(cntSecond.concat(selectedNum))
+            
+    //             if(cntSecond.length <= 2) {
+    //                 setCntSecond(cntSecond.substring(1))
+    //                 // document.getElementById('insert_second')?.blur()
+    //                 document.getElementById('insert_third')?.focus()
+    //             } 
 
+    //         } 
+    //     }
 
-
-    console.log('확인 넘', cntFirst)
+    // },[selectedNum])
 
     return(
         <div className="total_back">
@@ -98,7 +185,7 @@ const Ground: React.FC = () => {
                     <div className="player_located_last_base home_color"/>
                     <div className="ground_back_position_pitcher-base"/>
                     {gameStatus === 1 &&
-                    <button className="game_start" onClick={gameStart}>게임시작</button>
+                        <button className="game_start" onClick={gameStart}>게임시작</button>
                     }
                     <div className="game_pad">
                         {gameStatus === 2 &&
@@ -110,45 +197,52 @@ const Ground: React.FC = () => {
                         {gameStatus !== 1 &&
                         <>
                             <div className="game_pad_text">
-                                <input type="text"  
-                                onChange={(e) => setCntFirst(e.target.value)} 
-                                defaultValue={cntFirst} 
-                                className="game_pad_text_insert" 
-                                id="input_first"
-                                onKeyPress={(e) => {
+                                <input 
+                                    type="text"  
+                                    onChange={(e) => setCntFirst(e.target.value)} 
+                                    defaultValue={cntFirst} 
+                                    className="game_pad_text_insert" 
+                                    id="input_first"
+                                    readOnly
+                                    placeholder="0"
+                                    onKeyPress={(e) => {
                                     if (e.key === 'Backspace') {
                                         // registerTeam();
                                     }
                                     }}
-                            />
-                                <input type="text" onChange={(e) => setCntSecond(e.target.value)} className="game_pad_text_insert" id="input_second"/>
-                                <input type="text" onChange={(e) => setCntThird(e.target.value)} className="game_pad_text_insert" id="input_third"/>
+                                />
+                                {/* <input 
+                                    type="text" 
+                                    onChange={(e) => setCntSecond(e.target.value)} 
+                                    defaultValue={cntSecond} 
+                                    className="game_pad_text_insert" 
+                                    id="input_second"
+                                />
+                                <input 
+                                    type="text" 
+                                    onChange={(e) => setCntThird(e.target.value)} 
+                                    defaultValue={cntThird} 
+                                    className="game_pad_text_insert" 
+                                    id="input_third"
+                                /> */}
                             </div>
+                        { numObject.num.map((data) => (
                             <div className="game_pad_num_line">
-                                <button className="btn_num" onClick={selectNum} value="1">1</button>
-                                <button className="btn_num" onClick={selectNum}  value="2">2</button>
-                                <button className="btn_num" onClick={selectNum}  value="3">3</button>
+                                {data.no1 !=="" && <button className="btn_num" onClick={selectNum} value={`${data.no1}`}>{ data.no1 }</button>}
+                                    <button className="btn_num" onClick={selectNum} value={`${data.no2}`}>{data.no2}</button>
+                                {data.no3 !== "" &&  <button className="btn_num" onClick={selectNum} value={`${data.no3}`}>{data.no3}</button>}
                             </div>
-                            <div className="game_pad_num_line">
-                                <button className="btn_num" onClick={selectNum}  value="4">4</button>
-                                <button className="btn_num" onClick={selectNum}  value="5">5</button>
-                                <button className="btn_num" onClick={selectNum}  value="6">6</button>
-                            </div>
-                            <div className="game_pad_num_line">
-                                <button className="btn_num" onClick={selectNum} value="7">7</button>
-                                <button className="btn_num" onClick={selectNum} value="8">8</button>
-                                <button className="btn_num" onClick={selectNum} value="9">9</button>
-                            </div>
-                            <div className="game_pad_num_line">
-                                <button className="btn_num" onClick={selectNum} value="0">0</button>
-                            </div>
+                            ))}
                         </>
                         }
                         {gameStatus === 2 ?
                         <div className="game_pad_num_line">
-                            <button className="game_pad_num_line_info_register" onClick={gameStart}>등록</button>
+                            <button className="game_pad_num_line_info_register" onClick={() => toPrevNum()} >이전</button>
+                            <button className="game_pad_num_line_info_register" onClick={() => toNextNum()} >다음</button>
+                            <button className="game_pad_num_line_info_register" onClick={() => gameStart()}>등록</button>
                         </div>
                         : gameStatus === 3 ?
+                        // 게임 중일 때 사용될 등록버튼
                         <div className="game_pad_num_line">
                             <button className="game_pad_num_line_info_register">등록</button>
                         </div>
@@ -161,3 +255,8 @@ const Ground: React.FC = () => {
 }
 
 export default Ground;
+
+
+// 221112 구현해야할 부분
+// 백스페이스 버튼(번호 입력 중 수정을 위해) ---> pop으로 값 제거
+// 
