b430039d28a360791c320e045341d486418ecf57
BANGWON-LEE <qkddnjs31@gmail.com>
2023-04-15 12:54:54 +0900

    20230415 페이지 추가로 인한 코드 작성

--- game/src/App.tsx
+++ game/src/App.tsx
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
 import Ground from './components/Ground';
+import Enter from './components/Enter';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
       <BrowserRouter>
         <Routes>
-          <Route  element={<Main />} path="/" />
+          <Route  element={<Enter />} path="/" />
+          <Route  element={<Main />} path="/room" />
           <Route  element={<Ground />} path="/game" />
         </Routes>
       </BrowserRouter> 
