305c17b58802a4d514d30f9899f4718e083c0b47
BANGWON-LEE <qkddnjs31@gmail.com>
2023-04-17 23:26:32 +0900

    20230417 방선택 페이지 생성 -> 제외할 예정

--- game/src/components/Enter.tsx
+++ game/src/components/Enter.tsx
@@ -29,11 +29,44 @@ const Enter: React.FC = () => {
     
     ]  
   }
-
+  // let storedData : any;
+  // const [storedData, setStoredData] = useState<any>(0)
+  // const [roomFullCnt, setRoomFullCnt] = useState<number>(0)
+  // const [roomNum1, setRoomNum1] = useState<number>(0)
   // useEffect(() =>  {
+   
+  //   socket.on('1', (data) => {
+  //     console.log('data',data)
+  //     // if(data === 1 && roomNum1Status === 1){
+  //       // roomNum1Status = roomNum1Status + data;
+        
+  //       console.log('roomNum1Status', data)
+  //       setRoomNum1(data)
+  //       // data = 0;
+  //       // }
+        
+  //       setStoredData(localStorage.getItem("roomFullCnt1"));
+  //     })
+      
+  //     // return () => {
+  //       //   socket.off("roomFullCnt");
+  //   // };  
     
+  // },[]);
+  
+  // useEffect(() => {
+  //   if(Number(storedData) <= 2){
+  //   let roomNum1Status : number = 0;
+  //   console.log('roomNum1', roomNum1)
+  //   console.log('storeData', storedData)
+  //   roomNum1Status = Number(storedData) + roomNum1
+  //   console.log('로로로', roomNum1Status)
+  //   localStorage.setItem(`roomFullCnt1`, JSON.stringify(roomNum1Status));
+  //   // setRoomNum1(roomNum1Status)
+  //   }
+  // },[storedData])
 
-  // });
+  // console.log('storedData', storedData)
 
   const EnterRoom = () =>{
 
@@ -42,9 +75,11 @@ const Enter: React.FC = () => {
       <div className="project_title_change">
       {numObject.num.map((num : Line) => (
         console.log('dfd',num['no']),
-        <Link key={"room " + num['no']} to={`/room?no=${num['no']}`}>
-          <button className="project_title_change_team_name">{num['no'] +"번 방"}</button>          
-        </Link>
+        <Link key={"room " + num['no']} to={`/room?no=${num['no']}`} >
+          {/* { num['no'] === '1' && storedData === 2 ? null : */}
+          <button className="project_title_change_team_name"  >{num['no'] +"번 방"}</button>          
+          {/* } */}
+          </Link>
         ))
       }
       </div>
@@ -65,3 +100,6 @@ const Enter: React.FC = () => {
 };
 
 export default Enter;
+
+
+/// 로컬 스토로지 활용해서, 방에 들어와 있는 인원 수 체크 하여 disabled 하기
