b4078916a4a21d01396d8f2955d027ddeea8f1ad
BANGWON-LEE <qkddnjs31@gmail.com>
2023-05-12 17:50:39 +0900

    컴포넌트 구분 진행 중, 게임 플레이 화면 분할

---
+++ game/src/components/ground/GroundBase.tsx
@@ -0,0 +1,18 @@
+const GroundBase = () => {
+   
+   return (
+      <>
+         <div className="ground_back_position_top-base" />
+         <div className="player_located_first_base" />
+         <div className="ground_back_position_right-base" />
+         <div className="player_located_right_base" />
+         <div className="ground_back_position_left-base" />
+         <div className="player_located_left_base" />
+         <div className="ground_back_position_home-base" />
+         <div className="player_located_last_base home_color" />
+         <div className="ground_back_position_pitcher-base" />
+      </>
+   )
+};
+
+export default GroundBase;
