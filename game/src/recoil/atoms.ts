ee7df8b3be20c8e781f6c5b3af725e549fff1c22
BANGWON-LEE <qkddnjs31@gmail.com>
2023-05-20 10:56:57 +0900

    props로 넘긴 state recoil사용하여 전역으로 불러오도록 처리

--- game/src/recoil/atoms.ts
+++ game/src/recoil/atoms.ts
@@ -1,17 +1,21 @@
 import { atom } from "recoil"
 
-export const rivalTeamNameState = atom({
+export const rivalTeamNameState = atom<string>({
    key: 'rivalTeamNameState',
    default: '',
  });
 
- export const rivalTeamScoreState = atom({
+ export const rivalTeamScoreState = atom<number>({
    key: 'rivalTeamScoreState',
    default: 0,
  });
 
+ interface roomUserCntStateType {
+    room_no : string;
+    user_cnt : string;
+ }
 
- export const roomUserCntState = atom({
+ export const roomUserCntState = atom<roomUserCntStateType[]>({
   key: 'roomUserCntState',
   default: [
     {
@@ -20,3 +24,62 @@ export const rivalTeamNameState = atom({
     }
   ],
 });
+
+export const myTeamOriginId = atom<string | null>({
+  key: 'myTeamOriginId',
+  default: '',
+});
+
+export const matchHitCntGlobal = atom<number>({
+  key: 'matchHitCntGlobal',
+  default: 0,
+});
+
+
+export const matchBallCntGlobal = atom<number>({
+  key: 'matchBallCntGlobal',
+  default: 0,
+});
+
+
+
+export const matchStrikeCntGlobal = atom<number>({
+  key: 'matchStrikeCntGlobal',
+  default: 0,
+});
+
+export const determineMyAttackGlobal = atom<string[]>({
+  key: 'determineAttackGlobal',
+  default: ['','',''],
+});
+
+export const determineMyOutGlobal = atom<string[]>({
+  key: 'determineMyOutGlobal',
+  default: ['','',''],
+});
+
+export const gameRoundGlobal = atom<number | null >({
+  key: 'gameRoundGlobal',
+  default: 1,
+});
+
+export const myNumArrayGlobal = atom<string[] | null>({
+  key: 'myNumArrayGlobal',
+  default: ['','',''],
+});
+
+export const cntTextGlobal = atom<string>({
+  key: 'cntTextGlobal',
+  default: '',
+});
+
+export const gameStatusGlobal = atom<number | null>({
+  key: 'gameStatusGlobal',
+  default: 1,
+});
+
+export const gameSetCntGlobal = atom<number>({
+  key: 'gameSetCntGlobal',
+  default: 0,
+});
+
